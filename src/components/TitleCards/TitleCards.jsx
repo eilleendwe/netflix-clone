82b9c1b685055f21026773e4fb9d295f4b8fde12
eilleendwe <eilleenyeoh3@gmail.com>
2025-07-10 20:48:54 +0700

    Initial commit

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './TitleCards.css'
+
+const TitleCards = () => {
+  return (
+    <div className='titlecards'>
+      
+    </div>
+  )
+}
+import './TitleCards.css'
+
+export default TitleCards
